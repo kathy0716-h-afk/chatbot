396d89f3e3450e3b355084e39c8c920dc29c4e13
kathy0716-h-afk <khashimoto@go.gsis.kumamoto-u.ac.jp>
2026-05-17 20:08:25 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ else:
     model_name = st.selectbox(
         "使用する Gemini モデルを選択",
     (
-    "gemini-1.5-flash",
-    "gemini-1.5-pro"
+        "gemini-2.5-flash",
+        "gemini-2.5-pro"
     )
     )
     st.write(f"現在のモデル: **{model_name}**")
